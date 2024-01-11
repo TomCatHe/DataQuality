9425c1ff2e0798a92d60c8d4eef6553fae681c11
TomCatHe <heckerchong@gmail.com>
2024-01-11 17:32:41 +0800

    9

---
+++ src/test/java/com/dataquality/DataQualityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataquality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataQualityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
